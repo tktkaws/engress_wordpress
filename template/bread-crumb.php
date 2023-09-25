dc79e8a826eae2f51d18670773f3be66bd5ab822
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-25 11:36:38 +0900

    Add: パンくずリスト / ハンバーガーボタン

---
+++ template/bread-crumb.php
@@ -0,0 +1,8 @@
+<section class="l-bread-crumb">
+    <ul>
+        <li>ホーム</li>
+        <li>ブログ</li>
+        <li>カテゴリ1</li>
+        <li>ブログタイトル</li>
+    </ul>
+</section>
---
+++ template/nav-btn.php
@@ -0,0 +1,37 @@
+<button class="navbtn" onClick="document.querySelector('body').classList.toggle('open')">
+    <span class="navbtn-bar"></span>
+    <span class="sr-only">MENU</span>
+</button>
+
+<nav class="nav center">
+    <div class="container">
+        <ul class="menu">
+            <li>
+                <a href="/index.html">ホーム</a>
+            </li>
+            <li>
+                <a href="/newsIndex.html">お知らせ</a>
+            </li>
+            <li>
+                <a href="/blogIndex.html">ブログ</a>
+            </li>
+            <li>
+                <a href="/price.html">コース・料金</a>
+            </li>
+        </ul>
+        <button class="c-button --accent">
+            <a href="./contact.html" class="astro-VNZLVQNM">資料請求</a>
+        </button>
+        <button class="c-button --main">
+            <a href="./contact.html" class="astro-VNZLVQNM">お問い合わせ</a>
+        </button>
+        <h1 class="logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/logo@2x.png" />
+        </h1>
+
+        <address class="address">
+            <p>0123-456-7890</p>
+            <p>平日08:00~20:00</p>
+        </address>
+    </div>
+</nav>
