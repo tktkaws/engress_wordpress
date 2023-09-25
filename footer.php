6661ed9f8a58afb168437bc15ce46a558619923c
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-25 11:36:06 +0900

    Add: footer / cta

--- footer.php
+++ footer.php
@@ -1,4 +1,41 @@
-</main>
 <?php wp_footer(); ?>
+
+<footer class="l-footer">
+    <div class="l-footer-container">
+        <nav class="l-footer-nav">
+            <ul>
+                <li>
+                    <a href="/index.html">ホーム</a>
+                </li>
+                <li>
+                    <a href="/newsIndex.html">お知らせ</a>
+                </li>
+                <li>
+                    <a href="/blogIndex.html">ブログ</a>
+                </li>
+                <li>
+                    <a href="/price">コース・料金</a>
+                </li>
+            </ul>
+        </nav>
+        <address class="l-footer-address">
+            <h1 class="l-footer-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/logo@2x.png" />
+            </h1>
+            <p class="l-footer-call">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/tell_white.svg" alt="" />
+                <span>0123-456-7890</span>
+            </p>
+            <p class="l-footer-time">平日08:00~20:00</p>
+        </address>
+    </div>
+</footer>
+<section class="copywrite">
+    <div class="copywrite-container">
+        <p>© 2020 Engress.</p>
+    </div>
+</section>
+
 </body>
+
 </html>
---
+++ template/cta.php
@@ -0,0 +1,17 @@
+<section class="cta">
+    <div class="cta__bg-image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/cta.png" />
+        <div class="cta__bg-image-content">
+            <p class="cta__bg-image-title">まずは無料で資料請求から</p>
+            <button class="c-button-radius --cta">
+                <a href="./contact.html" class="astro-EMKAGK7Q">資料請求</a>
+            </button>
+            <p class="cta__bg-image-contact"><a href="contact"> お問い合わせ</a></p>
+        </div>
+    </div>
+    <address class="cta__only-text">
+        <p class="cta__only-text-left">お電話でのお問い合わせはこちら</p>
+        <p class="cta__only-text-center">0123-456-7890</p>
+        <p class="cta__only-text-right">平日 08:00~20:00</p>
+    </address>
+</section>
