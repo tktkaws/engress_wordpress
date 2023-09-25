1c14936d0916d0db0acf240dfb66f7aa3c5e76a8
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-25 11:39:17 +0900

    Add: blog-index-page

---
+++ page-blog-index.php
@@ -0,0 +1,168 @@
+<?php get_header(); ?>
+<main>
+    <?php get_template_part('template/bread-crumb') ?>
+    <section class="l-hero">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/other/blog.png" />
+        <h2 class="l-hero-title">ブログ</h2>
+    </section>
+    <section class="blog-index">
+        <h1 class="blog-index-title">新着一覧</h1>
+
+        <ul class="blog-index-list">
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample01.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample02.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample03.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample01.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample02.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample03.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample01.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample02.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample03.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+            <li class="blog-card">
+                <div class="blog-card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/sample01.png" />
+                    <p class="blog-card-tag">
+                        <span>カテゴリ1</span>
+                    </p>
+                </div>
+                <p class="blog-card-title">ブログタイトルテキストテキストテキストテキスト</p>
+                <time class="blog-card-date">2021-01-01</time>
+                <p class="blog-card-desc">
+                    記事の冒頭文テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </li>
+        </ul>
+        <div class="l-pagenation">
+            <ul class="l-pagenation-list">
+                <li class="l-pagenation-item">
+                    <a href="#" class="l-pagenation-link --active">1</a>
+                </li>
+                <li class="l-pagenation-item">
+                    <a href="#" class="l-pagenation-link">2</a>
+                </li>
+                <li class="l-pagenation-item">
+                    <a href="#" class="l-pagenation-link">3</a>
+                </li>
+                <li class="l-pagenation-item">
+                    <span>...</span>
+                </li>
+                <li class="l-pagenation-item">
+                    <a href="#" class="l-pagenation-link">5</a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <?php get_template_part('template/cta') ?>
+</main>
+
+
+
+<?php get_footer(); ?>
