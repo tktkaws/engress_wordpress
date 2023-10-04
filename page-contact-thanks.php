c87dc2a1da521183555635b964dbe488a3abe781
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-10-04 18:55:02 +0900

    Fix:問い合わせ完了　レイアウト変更

---
+++ page-contact-thanks.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<main>
+    <?php get_template_part('template/bread-crumb') ?>
+    <section class="contact">
+        <p>
+            お問い合わせいただきありがとうございます 内容を確認した後、担当者よりご連絡いたします
+        </p>
+        <a href="">ホームへ戻る</a>
+    </section>
+</main>
+
+<?php get_footer(); ?>
