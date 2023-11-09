3731c99b1bda0ea216d63278894c81ae7ab77490
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-11-09 11:26:38 +0900

    Fix:noindexを追加

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/scroll-hint@latest/css/scroll-hint.css">
     <?php wp_head(); ?>
