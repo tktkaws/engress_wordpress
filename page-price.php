d2912b8b74c72e08d9068fef3ed06b36eacb8b79
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-11-15 08:36:13 +0900

    Fix:不要な記述を削除

--- page-price.php
+++ page-price.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 <main>
-
     <section class="l-hero">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/other/plan.png" />
         <h2 class="l-hero-title">コース・料金</h2>
@@ -20,27 +19,6 @@
             Engressのカリキュラムは完全オーダーメイドのため、カリキュラム内のサポート内容によって料金が変動します。おすすめのスタンダードプランでは、進学先に合わせたサポートまで包括的に行います。
         </p>
     </section>
-    <!-- <section>
-        <div class="js-scrollable">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Col1</th>
-                        <th>Col2</th>
-                        <th>Col3</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Lorem ipsum dolor sit.</td>
-                        <td>Lorem ipsum dolor sit.</td>
-                        <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, magnam.</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </section> -->
-
     <section class="price-list">
         <h2 class="price-list-title">料金表</h2>
         <div class="js-scrollable">
@@ -52,7 +30,6 @@
                     'post_type' => 'plice_list',
                     "order" => "ASC"
                 );
-                console_log($args);
                 $myposts = get_posts($args);
                 foreach ($myposts as $post) : setup_postdata($post); ?>
                     <li class="price-list__card">
@@ -76,60 +53,6 @@
             </ul>
         </div>
     </section>
-    <!-- <section class="price-list">
-        <h2 class="price-list-title">料金表</h2>
-        <ul>
-            <li class="price-list__card">
-                <h3 class="price-list__card-title">
-                    <span>基礎プラン</span>
-                </h3>
-                <div class="price-list__card-body">
-                    <p class="price-list__card-price">59,000円~</p>
-                    <p class="price-list__card-desc">カリキュラム作成</p>
-                    <p class="price-list__card-desc">TOEFL学習サポート</p>
-                    <p class="price-list__card-desc">週一回のビデオMTG</p>
-                </div>
-            </li>
-            <li class="price-list__card">
-                <h3 class="price-list__card-title">
-                    <span>演習プラン</span>
-                </h3>
-                <div class="price-list__card-body">
-                    <p class="price-list__card-price">75,000円~</p>
-                    <p class="price-list__card-desc">カリキュラム作成</p>
-                    <p class="price-list__card-desc">TOEFL学習サポート</p>
-                    <p class="price-list__card-desc">週一回のビデオMTG</p>
-                    <p class="price-list__card-desc">月二回の模試（解説 付き）</p>
-                </div>
-            </li>
-            <li class="price-list__card --orange">
-                <h3 class="price-list__card-title --orange">
-                    <p>おすすめ</p>
-                    <p>志望校対策プラン</p>
-                </h3>
-                <div class="price-list__card-body">
-                    <p class="price-list__card-price --orange">95,000円~</p>
-                    <p class="price-list__card-desc">カリキュラム作成</p>
-                    <p class="price-list__card-desc">TOEFL学習サポート</p>
-                    <p class="price-list__card-desc">週一回のビデオMTG</p>
-                    <p class="price-list__card-desc">月二回の模試（解説 付き）</p>
-                    <p class="price-list__card-desc">週一の英語面接対策</p>
-                </div>
-            </li>
-            <li class="price-list__card">
-                <h3 class="price-list__card-title">
-                    <span>フレックスプラン</span>
-                </h3>
-                <div class="price-list__card-body">
-                    <p class="price-list__card-price">60,000円~</p>
-                    <p class="price-list__card-desc--none">＊別途ご相談ください</p>
-                </div>
-            </li>
-        </ul>
-    </section> -->
     <?php get_template_part('template/cta') ?>
 </main>
-
-
-
 <?php get_footer(); ?>
