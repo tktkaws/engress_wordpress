6d17b05433225dce6acb3214167d6373046cf201
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-11-15 12:59:23 +0900

    Fix:contact-thnaksレスポンシブ対応

--- page-contact-thanks.php
+++ page-contact-thanks.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
 <main>
     <?php get_template_part('template/bread-crumb') ?>
-    <section class="contact">
-        <div class="contact-container">
-            <p class="thanks-desc">
-                お問い合わせいただきありがとうございます
+    <section class="contact-thanks">
+        <div class="contact-thanks-wrapper">
+            <h1 class="contact-thanks-title">お問い合わせいただきありがとうございます
                 <br />
                 内容を確認した後、担当者よりご連絡いたします
+            </h1>
+            <p class="contact-thanks-link">
+                <a href="/">ホームへ戻る</a>
             </p>
-            <a href="/" class="thanks-link">ホームへ戻る</a>
 
         </div>
 
